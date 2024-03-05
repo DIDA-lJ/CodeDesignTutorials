8ace2206b8b9676a90ed5069050d8ce631351eab
LJ <97254796+DIDA-lJ@users.noreply.github.com>
2024-03-05 20:18:11 +0800

    Update VipVideoUserService.java

--- tutorials-2.1-1/src/main/java/cn/bugstack/design/impl/VipVideoUserService.java
+++ tutorials-2.1-1/src/main/java/cn/bugstack/design/impl/VipVideoUserService.java
@@ -10,11 +10,11 @@ import cn.bugstack.design.IVideoUserService;
  * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  */
 public class VipVideoUserService implements IVideoUserService {
-
+    @Override
     public void definition() {
         System.out.println("VIP用户，视频1080P蓝光");
     }
-
+    @Override
     public void advertisement() {
         System.out.println("VIP会员，视频无广告");
     }
